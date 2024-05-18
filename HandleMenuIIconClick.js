51ae6ec517124193d1db00dec9f09bb30154cbee
Aditya <adityadudhal97@gmail.com>
2024-05-18 16:33:30 +0530

    sidebar getting closed

--- HandleMenuIIconClick.js
+++ HandleMenuIIconClick.js
@@ -1,5 +1,11 @@
 function handleClick() {
-    console.log("Menu icon clicked")
+    console.log("Menu icon clicked");
+    document.getElementById("mobile-sidebar-id").style.width = "150px";
+}
+
+function handleClose() {
+    document.getElementById("mobile-sidebar-id").style.width = "0px";
 }
 
 document.getElementById("mobile-menu-button").addEventListener("click", handleClick)
+document.getElementById("mobile-menu-cross").addEventListener("click", handleClose)
